2fe4221299da746489d0dc62c71d66352c50d041
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2025-06-22 16:28:53 -0300

    Update logic auto-fit

--- auto-fit.sh
+++ auto-fit.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo "Hello World!!!"
+# Use fzf to select a branch from the list of git branches with preview of recent commits
+selected=$(git branch | fzf \
+    --height 40% \
+    --layout=reverse \
+    --border \
+    --preview 'git log --oneline $(echo {} | tr -d "* ")' \
+    --color bg:#222222,preview-bg:#333333
+)
 
+# Remove the leading '* ' if the branch is currently checked out
+selected=$(echo "$selected" | tr -d '* ')
 
+# Print the selected branch name
+echo "Switching to branch: $selected"
+
+# Switch to the selected branch
+git switch "$selected"
